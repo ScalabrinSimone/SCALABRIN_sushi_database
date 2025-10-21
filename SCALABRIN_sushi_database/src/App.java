2249a68822f8f5898759183e400a263a32729eea
SCALABRIN SIMONE <10933919@itisrossi.edu.it>
2025-10-21 12:29:20 +0200

    Pattern singleton

--- SCALABRIN_sushi_database/src/App.java
+++ SCALABRIN_sushi_database/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         try
         {
-            Database db = new Database();
+            Database db = Database.getInstance();
         }
         catch(Exception e)
         {
--- SCALABRIN_sushi_database/src/Database.java
+++ SCALABRIN_sushi_database/src/Database.java
@@ -1,13 +1,26 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import javax.xml.crypto.Data;
+
 public class Database {
     private Connection connection;
+    private static Database instance;
 
     public Database() throws Exception
     {
-        String url = "jdbc:sqlite:database/sushi.db";
+        String url = "database/sushi.db";
         connection = DriverManager.getConnection(url);
         System.out.println("Connsesso al database");
+    }
+
+    public static Database getInstance() throws Exception //Pattern singleton.
+    {
+        if (instance == null) 
+        {
+            instance = new Database();   
+        }
+
+        return instance;
     }
 }
